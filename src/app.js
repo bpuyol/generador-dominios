b2f8dbdd42caf27695f5bcfd557806ab7143c2a4
bpuyol <148617619+bpuyol@users.noreply.github.com>
2024-02-08 11:06:11 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,43 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+// script.js
+document.addEventListener("DOMContentLoaded", function() {
+  const pronouns = ["the", "our"];
+  const adjs = ["great", "big"];
+  const nouns = ["jogger", "racoon"];
+  const extensions = [".com", ".net", ".us", ".io"];
+  const domainList = document.getElementById("domainList");
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  function generateDomains() {
+    let domains = [];
+    for (let pronoun of pronouns) {
+      for (let adj of adjs) {
+        for (let noun of nouns) {
+          domains.push(generateDomain(pronoun, adj, noun));
+        }
+      }
+    }
+    return domains;
+  }
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  function generateDomain(pronoun, adj, noun) {
+    let domain = `${pronoun}${adj}${noun}`;
+    let extensionIndex = Math.floor(Math.random() * extensions.length);
+    let extension = extensions[extensionIndex];
+    // Check for domain hacks
+    if (domain.endsWith(extensions[extensionIndex].slice(1))) {
+      extension = extensions[extensionIndex].slice(1);
+    }
+    return `${domain}.${extension}`;
+  }
+
+  function displayDomains() {
+    let domains = generateDomains();
+    domainList.innerHTML = "";
+    domains.forEach(domain => {
+      let domainItem = document.createElement("p");
+      domainItem.textContent = domain;
+      domainList.appendChild(domainItem);
+    });
+  }
+
+  displayDomains();
+});
